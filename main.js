22406bb4a71b715c0005dd5e3ed4ab0fc27d9d58
Eric Kratochvil <Eric_Kratochvil@hks17.harvard.edu>
2017-01-12 15:30:23 -0500

    added the angry fox and moved written by eric on the bottom

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 document.querySelector('img').onclick = function() {
-    alert("Don't poke the fox!");
+    alert("I told you, Don't poke the fox!");
 }
 
+
+function  sayDontpoke() {
+	alert("you are warned, don't poke the fox")
+}
+
+sayDontpoke()
+
+//http://www.welivesecurity.com/wp-content/uploads/2014/08/Firefox-logo.png
+//https://s-media-cache-ak0.pinimg.com/236x/4a/02/6e/4a026efbde8336cf570632dc2a03b412.jpg
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'http://www.welivesecurity.com/wp-content/uploads/2014/08/Firefox-logo.png') {
+      myImage.setAttribute ('src','https://s-media-cache-ak0.pinimg.com/236x/4a/02/6e/4a026efbde8336cf570632dc2a03b412.jpg');
+    } else {
+      myImage.setAttribute ('src','http://www.welivesecurity.com/wp-content/uploads/2014/08/Firefox-logo.png');
+    }
+}
 
